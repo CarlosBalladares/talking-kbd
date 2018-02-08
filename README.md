8a611d72549fd8e855421b93a7c5082189b088f8
Carlos Balladares <carlos@balladares.co>
2018-02-08 07:01:50 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Talking keyboard
+
+
+This is a simple app that spells the letters you type on your keyboard.
+It also plays a weird sound when you press the space bar.
+
+This is based on this tutorial
+https://medium.freecodecamp.org/how-to-build-your-first-app-with-electron-41ebdb796930
+Read it.
+
+![Screenshot](screenshot.png)
+
+
+## To Use
+
+To clone and run this repository you'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+git clone https://github.com/carlosballadares/talking-kbd
+# Go into the repository
+cd talking-kbd
+# Install dependencies
+npm install
+# Run the app
+npm start
+```
---
+++ functions.js
@@ -0,0 +1,187 @@
+document.onkeydown = function(e){
+  switch (e.keyCode) {
+    case 32:
+        document.getElementById('SPACE').play();
+        document.getElementById('text').innerHTML='';
+        break;
+    case 48:
+        document.getElementById('0').play();
+        document.getElementById('text').innerHTML='0';
+        break;
+    case 49:
+        document.getElementById('1').play();
+        document.getElementById('text').innerHTML='1';
+
+        break;
+    case 50:
+        document.getElementById('2').play();
+        document.getElementById('text').innerHTML='2';
+
+        break;
+    case 51:
+        document.getElementById('3').play();
+        document.getElementById('text').innerHTML='3';
+
+        break;
+    case 52:
+        document.getElementById('4').play();
+        document.getElementById('text').innerHTML='4';
+
+        break;
+    case 53:
+        document.getElementById('5').play();
+        document.getElementById('text').innerHTML='5';
+
+        break;
+    case 54:
+        document.getElementById('6').play();
+        document.getElementById('text').innerHTML='6';
+
+        break;
+    case 55:
+        document.getElementById('7').play();
+        document.getElementById('text').innerHTML='7';
+
+        break;
+    case 56:
+        document.getElementById('8').play();
+        document.getElementById('text').innerHTML='8';
+
+        break;
+    case 57:
+        document.getElementById('9').play();
+        document.getElementById('text').innerHTML='9';
+
+        break;
+    case 65:
+        document.getElementById('A').play();
+        document.getElementById('text').innerHTML='A';
+
+        break;
+    case 66:
+        document.getElementById('B').play();
+        document.getElementById('text').innerHTML='B';
+
+        break;
+    case 67:
+        document.getElementById('C').play();
+        document.getElementById('text').innerHTML='C';
+
+        break;
+    case 68:
+        document.getElementById('D').play();
+        document.getElementById('text').innerHTML='D';
+
+        break;
+    case 69:
+        document.getElementById('E').play();
+        document.getElementById('text').innerHTML='E';
+
+        break;
+    case 70:
+        document.getElementById('F').play();
+        document.getElementById('text').innerHTML='F';
+
+        break;
+    case 71:
+        document.getElementById('G').play();
+        document.getElementById('text').innerHTML='G';
+
+        break;
+    case 72:
+        document.getElementById('H').play();
+        document.getElementById('text').innerHTML='H';
+
+        break;
+    case 73:
+        document.getElementById('I').play();
+        document.getElementById('text').innerHTML='I';
+
+        break;
+    case 74:
+        document.getElementById('J').play();
+        document.getElementById('text').innerHTML='J';
+
+        break;
+    case 75:
+        document.getElementById('K').play();
+        document.getElementById('text').innerHTML='K';
+
+        break;
+    case 76:
+        document.getElementById('L').play();
+        document.getElementById('text').innerHTML='L';
+
+        break;
+    case 77:
+        document.getElementById('M').play();
+        document.getElementById('text').innerHTML='M';
+
+        break;
+    case 78:
+        document.getElementById('N').play();
+        document.getElementById('text').innerHTML='N';
+
+        break;
+    case 79:
+        document.getElementById('O').play();
+        document.getElementById('text').innerHTML='O';
+
+        break;
+    case 80:
+        document.getElementById('P').play();
+        document.getElementById('text').innerHTML='P';
+
+        break;
+    case 81:
+        document.getElementById('Q').play();
+        document.getElementById('text').innerHTML='Q';
+
+        break;
+    case 82:
+        document.getElementById('R').play();
+        document.getElementById('text').innerHTML='R';
+
+        break;
+    case 83:
+        document.getElementById('S').play();
+        document.getElementById('text').innerHTML='S';
+
+        break;
+    case 84:
+        document.getElementById('T').play();
+        document.getElementById('text').innerHTML='T';
+
+        break;
+    case 85:
+        document.getElementById('U').play();
+        document.getElementById('text').innerHTML='U';
+
+        break;
+    case 86:
+        document.getElementById('V').play();
+        document.getElementById('text').innerHTML='V';
+
+        break;
+    case 87:
+        document.getElementById('W').play();
+        document.getElementById('text').innerHTML='W';
+
+        break;
+    case 88:
+        document.getElementById('X').play();
+        document.getElementById('text').innerHTML='X';
+
+        break;
+    case 89:
+        document.getElementById('Y').play();
+        document.getElementById('text').innerHTML='Y';
+
+        break;
+    case 90:
+        document.getElementById('Z').play();
+        document.getElementById('text').innerHTML='Z';
+
+        break;
+  }i
+};
